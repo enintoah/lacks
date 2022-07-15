c918ba19c3d581654072438ab4fe2bf6158cdccb
AA Student <appacademy@Mini-12.local>
2022-07-15 16:45:29 -0700

    started homepage css

--- frontend/components/homepage/homepage.jsx
+++ frontend/components/homepage/homepage.jsx
@@ -22,20 +22,30 @@ class Homepage extends React.Component {
     return (
     <div>
       <section className="first-homepage">
-        <nav>
-          <img src={window.white_logo} />
-          <a href="https://github.com/enintoah">Github</a>
-          <a href="https://www.linkedin.com/in/anthonie-lorsithong-551741232/">LinkedIn</a>
-          <NavLink to="login">Sign in</NavLink>
-          <button onClick={this.loginDemo}>DEMO</button>
-          <button onClick={this.signup}>TRY FOR FREE</button>
-        </nav>
+        <div className="homepage-navbar">
+          <div className="homepage-logo">
+            <img src={window.white_logo} />
+          </div>
+          <div className="homepage-links">
+            <a href="https://github.com/enintoah">Github</a>
+            <a href="https://www.linkedin.com/in/anthonie-lorsithong-551741232/">LinkedIn</a>
+          </div>
+          <div className="homepage-navbar-user-auth-links">
+            <NavLink to="login">Sign in</NavLink>
+            <button onClick={this.loginDemo}>DEMO</button>
+            <button onClick={this.signup}>TRY FOR FREE</button>
+          </div>
+        </div>
         <section>
           <h1>Great teamwork starts with a digital HQ</h1>
           <h2>With all your people, tools, and communication in one place, you can work faster and more flexibly than ever before.</h2>
           <NavLink to="/signup">SIGN UP WITH EMAIL</NavLink>
           <button onClick={this.loginDemo}>TRY A DEMO</button>
           <h2><strong>Slack is free to try for as long as you'd like</strong></h2>
+          <video autoPlay muted loop>
+            <source src="https://lacks-aa-dev.s3.us-west-1.amazonaws.com/homepage_1.mp4" type="video/mp4"/>
+          </video>
+            {/* Video is property of Slack */}
         </section>
       </section>
     </div>
