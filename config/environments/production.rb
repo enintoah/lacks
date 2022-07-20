1c266ca48b21c107fb73c590d3ff80a4570fd4cf
Anthonie Lorsithong <anthoniecorps@gmail.com>
2022-07-20 15:01:58 -0700

    trying to fix herkou

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,8 +46,12 @@ Rails.application.configure do
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
-  config.action_cable.url = "wss://#{ENV['RAILS_HOST']}/cable"
+  config.action_cable.url = "wss://#{ENV['RAILS_HOST']}/cable" 
   
+   config.action_cable.allowed_request_origins = [ 
+     'https://appacademylacks.herokuapp.com/',  
+     'https://appacademylacks.herokuapp.com/']
+
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
